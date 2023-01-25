88755409555c20593dcb5d51950439b962095334
Bera <bera@boraq-group.net>
2023-01-25 14:39:50 +0300

    fixing lints

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,7 @@
 """
 Sample tests
 """
-
 from django.test import SimpleTestCase
-
 from . import calc
 
 class CalcTests(SimpleTestCase):
@@ -12,12 +10,10 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
-
         self.assertEqual(res, 11)
 
     
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res =  calc.subtract(10, 15)
-
         self.assertEqual(res, 5)
